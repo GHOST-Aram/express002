6597cf381dfacbed1f28737708c30721a285e079
GHOST-Aram <owinoojala@gmail.com>
2023-07-09 12:06:51 +0300

    chore: Collect all app.js imports in one file        Creat app/utils.js

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
 const blogs = require('./fakeDb')
-const DataStore = require('./db/datastore')
 const dbURI = require('./dbCredentials')
-const Environment = require('./utils/environment')
-const express = require('express')
-const HttpResponse = require('./utils/httpResponse')
-const Middlewear = require('./utils/middlewear')
-const mongoose = require('mongoose')
-const Server = require('./utils/server')
+const {env, server,db, middlewear,response} = require('./app/utils')
 
-const app = express()
-
-const env = new Environment(app)
 env.setViewsDirectory('templates')
 env.setViewEngine('ejs')
 env.setStaticDir('./static')
 
-const server = new Server(app)
 const port = env.getPort()
 server.listen(port)
 
 
-const db = new DataStore(mongoose)
 db.connect(dbURI)
 
-const middlewear =  new Middlewear(app)
 middlewear.logRequest('tiny')
 
-const response = new HttpResponse(app)
 response.render('/','index', { title: 'Home', blogs } )
 response.render('/about', 'about', { title: 'About Us' })
 response.redirect('/about-us', '/about')
---
+++ app/utils.js
@@ -0,0 +1,16 @@
+const DataStore = require('../db/datastore')
+const Environment = require('../utils/environment')
+const express = require('express')
+const HttpResponse = require('../utils/httpResponse')
+const Middlewear = require('../utils/middlewear')
+const mongoose = require('mongoose')
+const Server = require('../utils/server')
+
+const app = express()
+const env = new Environment(app)
+const server = new Server(app)
+const db = new DataStore(mongoose)
+const middlewear =  new Middlewear(app)
+const response = new HttpResponse(app)
+
+module.exports = {env, server,db, middlewear,response}
