b2643858a502d414e7136e467022f3e1a068b350
GHOST-Aram <owinoojala@gmail.com>
2023-07-10 12:52:11 +0300

    refactor: Refactor blog.js

--- blog.js
+++ blog.js
@@ -20,36 +20,51 @@ app.post('/blogs', (req, res) =>{
    db.save(blog, () =>res.redirect('/'))
 })
 
+app.get('/blogs/create', (req, res) =>{
+    res.render('create-blog', { title: 'Create Blog'})
+})
+
 app.get('/blogs/:id', (req, res) =>{
     const id = req.params.id
     const blog = db.findOne(id)
     res.render('details', {blog, title: 'Blog Details'})
 })
 
-const blogs  = [
-    {   
-        _id: 1,
-        title: 'Tokyo Yushi', 
-        snippet: 'New Lifestyle dicovered in Tokyo',
-        body: 'More about Tokyo Yushi'
-    },
-    {
-        _id: 2,
-        title: 'Nairobi Set up for new inovations',
-        snippet: 'Find out about new interesting investments in Nairobi',
-        body: 'More about Investing inn Tech in the east african city'
-    },
-    {   
-        _id: 3,
-        title: 'New Flight to Duban',
-        snippet: 'KQ collaborating wit SA air to take you to and fro',
-        body: 'More about KQ and SA air collab'
-    }
-]
+app.get('/', (req, res) =>{
+    res.redirect('/blogs')
+})
 
-response.render('/blogs/create', 'create-blog', {title: 'Create Blog'})
-response.redirect('/', '/blogs')
-response.render('/blogs','index', { title: 'Blogs', blogs } )
-response.render('/about', 'about', { title: 'About Us' })
-response.redirect('/about-us', '/about')
-response.render404('404', { title: 'Page Not Found' })
+app.get('/blogs', (req, res) =>{
+    const blogs  = [
+        {   
+            _id: 1,
+            title: 'Tokyo Yushi', 
+            snippet: 'New Lifestyle dicovered in Tokyo',
+            body: 'More about Tokyo Yushi'
+        },
+        {
+            _id: 2,
+            title: 'Nairobi Set up for new inovations',
+            snippet: 'Find out about new interesting investments in Nairobi',
+            body: 'More about Investing inn Tech in the east african city'
+        },
+        {   
+            _id: 3,
+            title: 'New Flight to Duban',
+            snippet: 'KQ collaborating wit SA air to take you to and fro',
+            body: 'More about KQ and SA air collab'
+        }
+    ]
+    res.render('index', {title: 'Blogs', blogs})
+})
+app.get('/about', (req, res) =>{
+    res.render('about', { title: 'About Us'})
+})
+
+app.get('/about-us', (req, res) =>{
+    res.redirect('/about')
+})
+app.use((req, res) =>{
+    res.status(404).render('404', {title: 'Page Not Found'})
+})
+
