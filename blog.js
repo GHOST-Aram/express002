4a5e502890a031e3289de534bc10fddd674dca58
GHOST-Aram <owinoojala@gmail.com>
2023-07-10 12:39:21 +0300

    feat: Create blog details page

--- blog.js
+++ blog.js
@@ -6,33 +6,50 @@ env.setViewEngine('ejs')
 env.setStaticDir('./static')
 
 const port = env.getPort()
-db.connect(dbURI, () => server.listen(port))
+// db.connect(dbURI, 
+    // () => 
+    server.listen(port)
+    // )
 
 middlewear.logRequest('tiny')
 middlewear.encodeUrl({ extended: true })
 
-//Create blog from request
-// 
 
-// const blog = new Blog(
-    //     {
-        //         title: 'New Blog 2',
-        //         snippet: 'About My Blog 2',
-        //         body: 'More about my blog 2'
-        //     }
-        // )
-        // db.save(blog)
-        
 app.post('/blogs', (req, res) =>{
    const blog = db.createNew(Blog, req.body)
-   db.save(blog, ()=>res.redirect('/'))
+   db.save(blog, () =>res.redirect('/'))
 })
 
-const blogs  = []
+app.get('/blogs/:id', (req, res) =>{
+    const id = req.params.id
+    const blog = db.findOne(id)
+    res.render('details', {blog, title: 'Blog Details'})
+})
 
-response.render('/','index', { title: 'Home', blogs } )
+const blogs  = [
+    {   
+        _id: 1,
+        title: 'Tokyo Yushi', 
+        snippet: 'New Lifestyle dicovered in Tokyo',
+        body: 'More about Tokyo Yushi'
+    },
+    {
+        _id: 2,
+        title: 'Nairobi Set up for new inovations',
+        snippet: 'Find out about new interesting investments in Nairobi',
+        body: 'More about Investing inn Tech in the east african city'
+    },
+    {   
+        _id: 3,
+        title: 'New Flight to Duban',
+        snippet: 'KQ collaborating wit SA air to take you to and fro',
+        body: 'More about KQ and SA air collab'
+    }
+]
+
+response.render('/blogs/create', 'create-blog', {title: 'Create Blog'})
+response.redirect('/', '/blogs')
+response.render('/blogs','index', { title: 'Blogs', blogs } )
 response.render('/about', 'about', { title: 'About Us' })
 response.redirect('/about-us', '/about')
-response.redirect('/all-blogs', '/')
-response.render('/blogs/create', 'create-blog', {title: 'Create Blog'})
 response.render404('404', { title: 'Page Not Found' })
--- db/datastore.js
+++ db/datastore.js
@@ -3,39 +3,60 @@ class DataStore{
         this.dbAbstractor = dbAbstractor
     }
 
-    connect = (dbURI, callback) =>{
-        this.dbAbstractor.connect(
-            dbURI, { useNewUrlParser: true, useUnifiedTopology: true }
-        ).then((result) =>() =>{
-            callback
+    connect = async(dbURI, callback) =>{
+        try {
+            await this.dbAbstractor.connect(
+                dbURI, { useNewUrlParser: true, useUnifiedTopology: true }
+            )
             console.log('Woo!, Connected to DB:')
-        }).catch((error) => console.log(error))
+            callback
+        } catch (error) {
+            console.error(error)
+        }
+        // this.dbAbstractor.connect(
+        //     dbURI, { useNewUrlParser: true, useUnifiedTopology: true }
+        // ).then((result) =>() =>{
+        //     callback
+        // }).catch((error) => console.log(error))
     }
     createNew = (Model, data) =>{
         return new Model(data)
     }
-    findOne = (Id, Model) =>{
-        let item = {}
-        Model.findById(Id)
-        .then((result) => item =result)
-        .catch((error) => console.log(error))
-        
-        return item
+    findOne = async(Id, Model) =>{
+        try{
+            let item = await Model.findById(Id)
+            return item
+        } catch(error){
+            console.error(error)
+        }
     }
-    save = (modelInstance, callback) =>{
-        modelInstance.save().then((result) =>{
+    save = async(modelInstance, callback) =>{
+        try {
+            await modelInstance.save()
             callback
-        }).catch((error) => console.error(error))
+        } catch (error) {
+            console.error(error)
+        }
+        // modelInstance.save().then((result) =>{
+        //     callback
+        // }).catch((error) => console.error(error))
     }
     
-    findAll = (Model) =>{
-        let collection = []
-        Model.find().sort({ createdAt: -1 })
-        .then((result) => {
-            collection = result
-        }).catch((error) => console.log(error))
+    findAll = async(Model) =>{
+        try {
+            const collection =  await Model.find().sort({ createdAt: -1 })
+            return collection
+        
+        } catch (error) {
+            console.error(error)
+        }
+        // let collection = []
+        // Model.find().sort({ createdAt: -1 })
+        // .then((result) => {
+        //     collection = result
+        // }).catch((error) => console.log(error))
 
-        return collection
+        // return collection
     }   
 } 
 module.exports = DataStore
